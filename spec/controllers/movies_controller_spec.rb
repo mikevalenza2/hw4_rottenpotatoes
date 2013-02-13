03d51884ec82616ce723c14618db09dbb233f2f7
Mike Valenza <mvalenza@berkeley.edu>
2013-02-12 22:07:14 -0800

    new file:   controllers/movies_controller_spec.rb

---
+++ spec/controllers/movies_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe MoviesController do
+  describe 'Finding movies with same director' do
+    it 'should call the model method that finds movies with the same director'
+    it 'should select the Search Results template for rendering'
+    it 'should make the search results available to the template'
+  end
+end
+
